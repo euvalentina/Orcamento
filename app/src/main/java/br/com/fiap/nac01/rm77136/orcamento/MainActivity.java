014bd3ccc2e46b4f7d4aaa23a3cccc6a58b10f05
viicristina18@icloud.com <logonrm@fiap.com.br>
2017-03-09 10:48:36 -0300

    switch para verificação de qual radioButton esta selecionado e adicionado valor a cada um deles

--- app/src/main/java/br/com/fiap/nac01/rm77136/orcamento/MainActivity.java
+++ app/src/main/java/br/com/fiap/nac01/rm77136/orcamento/MainActivity.java
@@ -2,12 +2,56 @@ package br.com.fiap.nac01.rm77136.orcamento;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtResultado;
+    CheckBox frete;
+    RadioGroup grupoMaterial;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtResultado = (TextView) findViewById(R.id.txtResultado);
+        frete = (CheckBox) findViewById(R.id.frete);
+        grupoMaterial = (RadioGroup) findViewById(R.id.grupoMaterial);
+
+    }
+
+    public void Calcular(View view) {
+        double valor = 0;
+        int selected = grupoMaterial.getCheckedRadioButtonId();
+
+        switch (selected){
+            case R.id.pisoBranco:
+                valor = 24.9;
+                txtResultado.setText(String.valueOf(valor));
+                break;
+            case R.id.pisoAlbania:
+                valor = 11.9;
+                txtResultado.setText(String.valueOf(valor));
+                break;
+            case R.id.pisoPorcelanato:
+                valor = 39.9;
+                txtResultado.setText(String.valueOf(valor));
+                break;
+            case R.id.pisoMosaico:
+                valor = 16.9;
+                txtResultado.setText(String.valueOf(valor));
+                break;
+            default:
+                Toast.makeText(this, R.string.informe3, Toast.LENGTH_SHORT).show();
+        }
+
+
+
     }
 }
