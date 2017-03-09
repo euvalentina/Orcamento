67f1eec4bcb8c5427efcfa4c75f5e3d0913234c1
viicristina18@icloud.com <logonrm@fiap.com.br>
2017-03-09 10:10:46 -0300

    primeiro commit da nac1

---
+++ app/src/main/java/br/com/fiap/nac01/rm77136/orcamento/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.fiap.nac01.rm77136.orcamento;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
